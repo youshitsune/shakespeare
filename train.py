6958ddf856c7390b533383830dbb545deec5d5ec
youshitsune <luka@youshitsune.me>
2023-02-21 07:25:49 +0100

    Make train.py to run on CPU and GPU

--- train.py
+++ train.py
@@ -2,17 +2,30 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-batch_size = 64
-block_size = 256
-max_iters = 5000
-eval_interval = 500
-learning_rate = 3e-4
 device = "cuda" if torch.cuda.is_available() else "cpu"
-eval_iters = 200
-n_embd = 384
-n_head = 6
-n_layer = 6
-dropout = 0.2
+
+if device == "cuda":
+    batch_size = 64
+    block_size = 256
+    max_iters = 5000
+    eval_interval = 500
+    learning_rate = 3e-4
+    eval_iters = 200
+    n_embd = 384
+    n_head = 6
+    n_layer = 6
+    dropout = 0.2
+else:
+    batch_size = 16
+    block_size = 32
+    max_iters = 5000
+    eval_interval = 100
+    learning_rate = 1e-3
+    eval_iters = 200
+    n_embd = 64
+    n_head = 4
+    n_layer = 4
+    dropout = 0.0
 
 with open("input.txt", "r", encoding="utf-8") as f:
     text = f.read()
